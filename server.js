fc1eb0237329a1e2ae53a291bae7c0a80d709c04
Krystian Kluba <klubakrystian@gmail.com>
2026-01-27 12:24:20 +0100

    middleware

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(cors());
+
+const db = [
+  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+  {
+    id: 2,
+    author: 'Amanda Doe',
+    text: 'They really know how to make you happy.',
+  },
+];
+
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });

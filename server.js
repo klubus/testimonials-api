999a340a972dd673772f29f21bbc102343ea365b
Krystian Kluba <klubakrystian@gmail.com>
2026-01-28 11:50:32 +0100

    build

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const { randomUUID } = require('crypto');
 var cors = require('cors');
-const db = require('./db');
+const path = require('path');
 
 const app = express();
 
@@ -18,10 +17,17 @@ app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
